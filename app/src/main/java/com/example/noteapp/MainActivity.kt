d38cc6a32424ee546f4865f862b2c79f510a463a
user <tentievazoma@gmail.com>
2024-04-15 19:27:18 +0600

    05

--- app/src/main/java/com/example/noteapp/MainActivity.kt
+++ app/src/main/java/com/example/noteapp/MainActivity.kt
@@ -1,11 +1,32 @@
-package com.example.noteapp
+package com.geeks.noteapp.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.noteapp.Pref
+import com.example.noteapp.R
+import com.example.noteapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container)as NavHostFragment
+
+        navController= navHostFragment.navController
+
+        if (Pref.isOnBoardShown){
+            navController.navigate(R.id.noteFragment)
+        }else{
+            navController.navigate(R.id.onBoardFragment)
+        }
     }
 }
